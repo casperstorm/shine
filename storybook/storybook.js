f7d52d5881239df2482424444d042d08f0c1ff1c
Casper Rogild Storm <casperstorm@me.com>
2017-12-01 14:28:30 +0100

    implemented first step of storybook

---
+++ storybook/storybook.js
@@ -0,0 +1,33 @@
+/* eslint-disable global-require */
+import React, { Component } from 'react'
+import { getStorybookUI, configure } from '@storybook/react-native'
+import { Navigation } from 'react-native-navigation'
+
+// import stories
+configure(() => {
+  require('./stories')
+}, module)
+
+// This assumes that storybook is running on the same host as your RN packager,
+// to set manually use, e.g. host: 'localhost' option
+const StorybookUIRoot = getStorybookUI({ port: 7007 })
+
+// react-native hot module loader must take in a Class - https://github.com/facebook/react-native/issues/10991
+// https://github.com/storybooks/storybook/issues/2081
+// eslint-disable-next-line react/prefer-stateless-function
+class StorybookUIHMRRoot extends Component {
+  render() {
+    return <StorybookUIRoot />
+  }
+}
+
+Navigation.registerComponent('shine', () => StorybookUIHMRRoot)
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'shine',
+    title: 'Storybook',
+  },
+})
+
+export default StorybookUIHMRRoot
