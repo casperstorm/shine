c39bfe32063c58234442f19dd06919f79434d722
Casper Rogild Storm <casperstorm@me.com>
2017-12-05 20:23:02 +0100

    fixed so it is working on android

--- android/app/src/main/java/com/cryptoshine/MainApplication.java
+++ android/app/src/main/java/com/cryptoshine/MainApplication.java
@@ -2,6 +2,7 @@ package com.cryptoshine;
 
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,7 +19,7 @@ public class MainApplication extends NavigationApplication {
     // Add additional packages you require here
     // No need to add RnnPackage and MainReactPackage
     return Arrays.<ReactPackage>asList(
-            // eg. new VectorIconsPackage()
+      new ReactNativeConfigPackage()
     );
   }
 
