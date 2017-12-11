a1fdbc89cd316033ec7241406b4704147175970b
Casper Rogild Storm <casperstorm@me.com>
2017-12-11 11:15:15 +0100

    added new tests

--- src/store/news/__tests__/index.test.js
+++ src/store/news/__tests__/index.test.js
@@ -1,10 +1,17 @@
+import _ from 'lodash'
+
 import reducer, { initialState } from '../reducer'
-import { greetings, updatedDate } from '../actions'
+import { greetings, refreshDate } from '../actions'
 import { Greetings } from '../../../utils/greetings'
+import * as selectors from '../../selectors'
 
 describe('reducer', () => {
   it('should return initial state', () => {
     expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('should not affect state', () => {
+    expect(reducer(undefined, { type: 'NOT_EXISTING' })).toEqual(initialState)
   })
 
   it(`should respond to greetings action`, () => {
@@ -15,12 +22,37 @@ describe('reducer', () => {
     })
   })
 
-  it(`should respond to updatedDate action`, () => {
+  it(`should respond to refreshDate action`, () => {
     const date = new Date()
-    const action = updatedDate(date)
+    const action = refreshDate(date)
     expect(reducer(undefined, action)).toEqual({
       ...initialState,
       date: date,
     })
+  })
+})
+
+describe('selector', () => {
+  const date = new Date()
+  const items = [{ foo: 'bar' }]
+  const greetings = ['foobar']
+  const state = {
+    news: {
+      date: date,
+      items: items,
+      greetings: greetings,
+    },
+  }
+
+  it('should return greeting', () => {
+    expect(selectors.selectRandomGreetings(state)).toEqual(_.first(greetings))
+  })
+
+  it('should return date', () => {
+    expect(selectors.newsUpdatedDate(state)).toEqual(date)
+  })
+
+  it('should return items', () => {
+    expect(selectors.sortedNewsItems(state)).toEqual(items)
   })
 })
