e325b95cdd7c6a500bcf99ab4361adc379115c2d
Casper Rogild Storm <casperstorm@me.com>
2017-12-12 12:42:35 +0100

    added more redux tests

--- src/store/news/__tests__/index.test.js
+++ src/store/news/__tests__/index.test.js
@@ -1,9 +1,14 @@
 import _ from 'lodash'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
 
 import reducer, { initialState } from '../reducer'
-import { greetings, refreshDate } from '../actions'
 import { Greetings } from '../../../utils/greetings'
 import * as selectors from '../../selectors'
+import { actionTypes, itemsFetch, refreshDate, greetings } from '../actions'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
 
 describe('reducer', () => {
   it('should return initial state', () => {
@@ -14,11 +19,11 @@ describe('reducer', () => {
     expect(reducer(undefined, { type: 'NOT_EXISTING' })).toEqual(initialState)
   })
 
-  it(`should respond to greetings action`, () => {
-    const action = greetings()
+  it(`should respond to itemsFetch action`, () => {
+    const action = { type: actionTypes.NEWS_FETCH, value: [] }
     expect(reducer(undefined, action)).toEqual({
       ...initialState,
-      greetings: Greetings,
+      items: [],
     })
   })
 
@@ -28,6 +33,45 @@ describe('reducer', () => {
     expect(reducer(undefined, action)).toEqual({
       ...initialState,
       date: date,
+    })
+  })
+
+  it(`should respond to greetings action`, () => {
+    const action = greetings()
+    expect(reducer(undefined, action)).toEqual({
+      ...initialState,
+      greetings: Greetings,
+    })
+  })
+})
+
+describe('actions', () => {
+  it(`should fetch items correctly`, async () => {
+    const items = [{ title: 'foobar' }]
+    const store = mockStore({ items: [] })
+    fetch.mockResponse(JSON.stringify({ results: items }))
+
+    store.dispatch(itemsFetch()).then(() => {
+      expect(store.getActions()).toEqual({
+        type: actionTypes.NEWS_FETCH,
+        value: items,
+      })
+    })
+  })
+
+  it(`should add new date`, () => {
+    const date = new Date()
+    expect(refreshDate(date)).toEqual({
+      type: actionTypes.NEWS_DATE,
+      value: date,
+    })
+  })
+
+  it(`should have all greetings`, () => {
+    const data = Greetings
+    expect(greetings()).toEqual({
+      type: actionTypes.NEWS_GREETINGS,
+      value: data,
     })
   })
 })
@@ -44,15 +88,15 @@ describe('selector', () => {
     },
   }
 
-  it('should return greeting', () => {
-    expect(selectors.selectRandomGreetings(state)).toEqual(_.first(greetings))
+  it('should return items', () => {
+    expect(selectors.sortedNewsItems(state)).toEqual(items)
   })
 
   it('should return date', () => {
     expect(selectors.newsUpdatedDate(state)).toEqual(date)
   })
 
-  it('should return items', () => {
-    expect(selectors.sortedNewsItems(state)).toEqual(items)
+  it('should return greeting', () => {
+    expect(selectors.selectRandomGreetings(state)).toEqual(_.first(greetings))
   })
 })
