710061e83f3e123c78cd6671d0d655af0301118d
Casper Rogild Storm <casperstorm@me.com>
2018-01-18 10:20:17 +0100

    implemented redux-persist

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,11 +1,19 @@
 /* @flow */
 import { Platform } from 'react-native'
-import { combineReducers, applyMiddleware, compose } from 'redux'
+import { applyMiddleware, compose } from 'redux'
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistCombineReducers } from 'redux-persist'
 import { composeWithDevTools } from 'remote-redux-devtools' //eslint-disable-line
 import Reactotron from 'reactotron-react-native'
 import thunk from 'redux-thunk'
 import reducers from './reducers'
 import type { State } from './types'
+
+const persistConfig = {
+  key: '@Shine:store',
+  storage,
+  whitelist: ['theme'],
+}
 
 export default function configureStore(onStoreReady: () => void, initialState?: State) {
   const middleware = [thunk]
@@ -17,8 +25,10 @@ export default function configureStore(onStoreReady: () => void, initialState?: 
   })
   const enhancerBase = compose(applyMiddleware(...middleware))
   const enhancer = __DEV__ ? devToolsComposeEnhancers(enhancerBase) : enhancerBase
-  const reducer = combineReducers(reducers)
+  const reducer = persistCombineReducers(persistConfig, reducers)
   const store = Reactotron.createStore(reducer, initialState, enhancer)
+
+  persistStore(store, {}, onStoreReady)
 
   return store
 }
